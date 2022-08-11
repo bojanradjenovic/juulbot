acc1a01410861b33a3980736b6b7b764d7ca1fa5
Bojan Rađenović <bojanradjenovic@protonmail.com>
2022-08-11 15:57:05 +0200

    Update urban.py
    Embed + error if definition doesn't exist

--- commands/urban.py
+++ commands/urban.py
@@ -18,9 +18,19 @@ async def urban(ctx, *, word):
     logger.info(f"{ctx.author.name}#{ctx.author.discriminator} has ran '{ctx.command}' in guild '{ctx.guild}' with message '{ctx.message.content}'!\n")
     querystring = {"term":word}
     response = requests.request("GET", url, headers=headers, params=querystring).json()
-    print(response)
-    await ctx.send(response['list'][0]['definition'])
-
+    embed=discord.Embed(title=f"Urban Dictionary - '{word}'!")
+    embed.url = f"{response['list'][0]['permalink']}"
+    embed.description = f"``{response['list'][0]['definition']}``\n 👍 - {response['list'][0]['thumbs_up']}\n Author - ``{response['list'][0]['author']}``"
+    embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+    embed.colour = ctx.author.colour
+    await ctx.send(embed=embed)
+@urban.error
+async def urban_error(ctx, error):
+    embed=discord.Embed(title=f"Urban dictionary!")
+    embed.description = f"Unable to retrieve definition (doesn't exist or API died lol)"
+    embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+    embed.colour = ctx.author.colour
+    await ctx.send(embed=embed)
 
 
 def setup(bot):
