d019e0a25b38f7932ced7cea02ccd3ec4abd54e6
Bojan Rađenović <github@radjenovic.dev>
2022-08-20 18:49:46 +0200

    added ping command
    first time using cogs (still dont know how 2 cog properly 😭, should modify everything else 2 use cogs tbh but idk)

---
+++ commands/ping.py
@@ -0,0 +1,24 @@
+import discord 
+from discord.ext import commands
+import logging
+logger = logging.getLogger('discord.ext')
+handler = logging.FileHandler(filename='commands.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+logging.basicConfig(level=logging.INFO)
+
+class ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command()
+    async def ping(self, ctx):
+        logger.info(f"{ctx.author.name}#{ctx.author.discriminator} has ran '{ctx.command}' in guild '{ctx.guild}' with message '{ctx.message.content}'!\n")
+        embed=discord.Embed(title=f"Pong!")
+        embed.description = f"{round(self.bot.latency * 1000)}ms"
+        embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
+        embed.colour = ctx.author.colour
+        await ctx.send(embed=embed)
+def setup(bot):
+    bot.add_cog(ping(bot))
