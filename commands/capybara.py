3097b21356fd7eb0afbb3cc9011f006f36001388
Bojan Rađenović <github@radjenovic.dev>
2022-09-05 22:19:33 +0200

    i dont know what noise capybara make
    - added command that sends an image of a random capybara
    - also deleted an unused import that i didnt notice the first time around smh

--- commands/capybara.py
+++ commands/capybara.py
@@ -5,31 +5,31 @@ import requests
 logger = logging.getLogger('discord.ext')
 
 @commands.command()
-async def cat(ctx):
+async def capybara(ctx):
     logger.info(f"{ctx.author.name}#{ctx.author.discriminator} has ran '{ctx.command}' in guild '{ctx.guild}' with message '{ctx.message.content}'!\n")
-    response = requests.request("GET", f"https://cataas.com/cat?json=true")
+    response = requests.request("GET", f"https://api.capy.lol/v1/capybara?json=true")
     if response.status_code != 200:
-            embed=discord.Embed(title=f"Random cat!")
-            embed.description = f"Unable to retrieve a cat. Please try again later."
+            embed=discord.Embed(title=f"Random capybara!")
+            embed.description = f"Unable to retrieve a capybara. Please try again later."
             embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
             embed.colour = ctx.author.colour
             await ctx.send(embed=embed)
             return
 
     json = response.json()
-
-    embed=discord.Embed(title=f"Random cat!")
-    embed.set_image(url=f"https://cataas.com/{json['url']}")
+    
+    embed=discord.Embed(title=f"Random capybara!")
+    embed.set_image(url=f"{json['data']['url']}")
     embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
     embed.colour = ctx.author.colour
     await ctx.send(embed=embed)
-@cat.error
-async def cat_error(ctx, error):
-     embed=discord.Embed(title=f"Random cat!")
-     embed.description = f"Unable to retrieve a cat. Please try again later."
+@capybara.error
+async def capybara_error(ctx, error):
+     embed=discord.Embed(title=f"Random capybara!")
+     embed.description = f"Unable to retrieve a capybara. Please try again later."
      embed.set_footer(text=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.avatar_url)
      embed.colour = ctx.author.colour
      await ctx.send(embed=embed)
 
 def setup(bot):
-    bot.add_command(cat)
+    bot.add_command(capybara)
